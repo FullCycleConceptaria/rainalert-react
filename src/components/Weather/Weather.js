1423caa7fedbc08ac34643337dbd4e8924f54538
NiteChill <puissantachille@gmail.com>
2023-01-15 21:57:53 +0100

    margin fix

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,17 +1,19 @@
-import styles from './Weather.module.scss'
+import styles from './Weather.module.scss';
 
-export default function Weather(){
-    return(
-        <div className={styles.weather}>
-            <div className={styles.weatherBox}>
-                <div className={styles.weatherBoxImg}>
-                    <i class="fa-solid fa-cloud fa-4x"></i>
-                </div>
-                <div className={styles.weatherBoxTemperature}>15°C</div>
-            </div>
-            <div className={styles.weatherMillimeters}>
-                <div className={styles.weatherMillimetersTxt}>La pluie se termine dans 1h45</div>
-            </div>
+export default function Weather() {
+  return (
+    <div className={styles.weather}>
+      <div className={styles.weatherBox}>
+        <div className={styles.weatherBoxLogo}>
+          <i class='fa-solid fa-cloud fa-4x'></i>
         </div>
-    )
+        <div className={styles.weatherBoxTemperature}>15°C</div>
+      </div>
+      <div className={styles.weatherMillimeters}>
+        <div className={styles.weatherMillimetersTxt}>
+          La pluie se termine dans 1h45
+        </div>
+      </div>
+    </div>
+  );
 }
